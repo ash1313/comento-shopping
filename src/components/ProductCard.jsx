594332b1a57492348842ee9a365dd841e8424119
ash1313 <ash1313@naver.com>
2022-12-14 02:34:25 +0900

    feat: Home 페이지 디자인 적용

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+const ProductCard = ({ name, description, thumbnail }) => {
+  return (
+    <ProductCardStyled>
+      <ProductThumbnail style={{ backgroundImage: `url(${thumbnail})` }} />
+      <ProductName>{name}</ProductName>
+      <ProductDesc>{description}</ProductDesc>
+    </ProductCardStyled>
+  );
+};
+const ProductCardStyled = styled.div`
+  padding-bottom: 40px;
+`;
+
+const ProductThumbnail = styled.div`
+  width: 100%;
+  height: 204px;
+  background-size: cover;
+  background-position: center;
+`;
+const ProductName = styled.div`
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 26px;
+  letter-spacing: -0.01em;
+  padding-bottom: 12px;
+`;
+const ProductDesc = styled.div`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 21px;
+  letter-spacing: -0.01em;
+  color: #000000;
+`;
+
+export default ProductCard;
